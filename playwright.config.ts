628112e06ae8769a1429b245d9bbc88704901069
Sukhwinder-003 <3sukhwinder@gmail.com>
2026-07-02 23:08:28 +0100

    update config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
     screenshot: 'on-first-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
-    headless: false
+    headless: true,
   },
 
   /* Configure projects for major browsers */
